43a22e4149cbe0e80976a116ddb0ef2017c62c92
Adrien <39885692+1-irdA@users.noreply.github.com>
2022-10-20 22:22:28 +0200

    Add Telegram

--- main.py
+++ main.py
@@ -8,33 +8,48 @@ from dotenv import load_dotenv
 FILENAME = 'edt.pdf'
 JSON_FILE = 'last.json'
 
-def convert_obj_to_datetime(obj):
+def convert_obj_to_datetime(obj) -> datetime:
     # convert to str and remove +0200
-    dt_str = str(obj).replace("'", "").split('+')[0]
+    dt_str = str(obj).replace("'", '').split('+')[0]
     # convert pikepdf date to datetime
     return datetime.strptime(dt_str, 'D:%Y%m%d%H%M%S')
 
-def get_last_update_dt():
-    with open(JSON_FILE, "r+") as file:
+def get_last_update_dt() -> datetime:
+    with open(JSON_FILE, 'r+') as file:
         content = json.load(file)
         return datetime.strptime(content['last']['mod_date'], '%Y-%m-%d %H:%M:%S')
 
-def insert_json(info):
+def insert_json(info) -> None:
     dictionary = {
         'author': str(info.Author),
         'mod_date': str(convert_obj_to_datetime(info.ModDate))
     }
-    with open(JSON_FILE, "r+") as file:
+    with open(JSON_FILE, 'r+') as file:
         content = json.load(file)
         content['last'].update(dictionary)
         file.seek(0)
         json.dump(content, file, indent=4)
 
+def send_telegram_msg(info, schedule_url: str, mod_dt: datetime) -> None:
+    token = os.getenv('TOKEN')
+    chat_id = os.getenv('CHAT_ID')
+    telegram_url = f'https://api.telegram.org/bot{token}/sendMessage'
+
+    payload = {
+        'text': f'{str(info.Author)} has updated the university schedule at {mod_dt}. {schedule_url}',
+        'chat_id': chat_id
+    }
+    headers = {
+        'accept': 'application/json',
+        'content-type': 'application/json'
+    }
+    requests.post(telegram_url, json=payload, headers=headers)
+
 def main():
     session = os.getenv('SESSION')
     url = os.getenv('URL')
     cookies = {'MoodleSession': session}
-    response = requests.get(URL, cookies=cookies)
+    response = requests.get(url, cookies=cookies)
     open(FILENAME, 'wb').write(response.content)
     
     try:
@@ -45,7 +60,7 @@ def main():
 
         if curr_mod_dt > last_mod_dt:
             insert_json(info) 
-            print('Diff')
+            send_telegram_msg(info, url, curr_mod_dt)
 
     except Exception as e:
         print(e)
